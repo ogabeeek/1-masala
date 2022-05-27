8b5ec7fe88b7720528503575c21d8e14087943d9
ogabeeek <101085164+ogabeeek@users.noreply.github.com>
2022-05-27 16:20:44 +0500

    bbb

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const elResult= document.querySelector(".result");
+const elResult = document.querySelector(".result");
 
-const elPromt1= Number(prompt("birinchi sonni kiriting"))
-const elPromt2= Number(prompt("ikkinchi sonni kiriting"))
+const elPromt1 = Number(prompt("birinchi sonni kiriting"))
+const elPromt2 = Number(prompt("ikkinchi sonni kiriting"))
 
 
-const a=[]
-var sum=0
+const a = []
+var sum = 0
 
 a.push(elPromt1)
 a.push(elPromt2)
@@ -13,40 +13,11 @@ a.push(elPromt2)
 console.log(a);
 
 
-if((typeof elPromt1  === "string")&& (typeof elPromt2  === "string")){
-     alert("Iltimos son kiritng ")
-}else{
-    for(i of a){
-        sum +=i
-        elResult.textContent=(sum)
+if ((typeof elPromt1 === "string") && (typeof elPromt2 === "string")) {
+    alert("Iltimos son kiritng ")
+} else {
+    for (i of a) {
+        sum += i
+        elResult.textContent = (sum)
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
